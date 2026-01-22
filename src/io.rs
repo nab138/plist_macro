a12f95e2b94eebc24ea35e7c2253018b2b66aa2d
Jackson Coxson <jkcoxson@gmail.com>
2026-01-22 15:17:23 -0700

    Actually change function signature
    a

--- src/io.rs
+++ src/io.rs
@@ -25,7 +25,7 @@ pub fn plist_to_xml_bytes(p: &plist::Dictionary) -> Vec<u8> {
     writer.into_inner().unwrap()
 }
 
-pub fn plist_value_to_xml_bytes(p: &plist::Dictionary) -> Vec<u8> {
+pub fn plist_value_to_xml_bytes(p: &plist::Value) -> Vec<u8> {
     let buf = Vec::new();
     let mut writer = std::io::BufWriter::new(buf);
     plist::to_writer_xml(&mut writer, &p).unwrap();
